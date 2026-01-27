394404d680cbd2f67f85dbd0771be0c17d9c6624
Andi <andig2413@gmail.com>
2026-01-27 17:00:44 +0100

    Added footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='mb-0 mt-20 py-2 text-center font-semibold text-lg'>ACME 2026</footer>
+  )
+}
+
+export default Footer
